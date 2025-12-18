74b9aa2192ef0a20552001c51421534f2035303f
Benjamin Davies <bentendavies@gmail.com>
2025-12-19 12:13:15 +1300

    feat: output to file

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,18 @@ use diameter::{charts::Chart, scales::Scale};
 struct Cli {
     /// The ChordPro file to process
     input: PathBuf,
+    /// The output file (defaults to stdout)
+    #[arg(short, long)]
+    output: Option<PathBuf>,
     /// Output chords inline with lyrics
     #[arg(short, long)]
     inline: bool,
-    /// Convert letter chords to numbers
-    #[arg(short, long)]
-    numbers: bool,
     /// Transpose the song into a different key
     #[arg(short, long)]
     key: Option<Scale>,
+    /// Convert letter chords to numbers
+    #[arg(short, long)]
+    numbers: bool,
 }
 
 fn main() {
@@ -27,12 +30,16 @@ fn main() {
         .expect("unable to parse ChordPro file");
 
     chart.set_inline(cli.inline);
-    if cli.numbers {
-        chart.to_numbers();
-    }
     if let Some(new_key) = cli.key {
         chart.transpose_to(new_key);
     }
+    if cli.numbers {
+        chart.to_numbers();
+    }
 
-    print!("{chart}");
+    if let Some(output) = cli.output {
+        fs::write(output, chart.to_string()).expect("unable to write output file");
+    } else {
+        print!("{chart}");
+    }
 }
